5c66232866fa30d81f2eece15ac277a47a4bf05f
Alfonso Vásquez <avasquez@rivetlogic.com>
2014-05-31 17:48:17 -0600

    Added api module

---
+++ api/src/main/java/org/intellitraffic/TrafficIncident.java
@@ -0,0 +1,10 @@
+package org.intellitraffic;
+
+/**
+ * Represents a traffic incident (accident, construction, etc.)
+ *
+ * @author avasquez
+ * @author mariobarque
+ */
+public class TrafficIncident {
+}
